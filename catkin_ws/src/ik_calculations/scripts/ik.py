5b7fc68e1d0cd8a8e250e564cb82dc1f6872e3ec
SamS1205 <snspeer@andrew.cmu.edu>
2018-04-16 17:06:08 -0400

    Updated IK
    Ik now does not include y base position, turntable position, and references off top right corner for (0, 0)

--- catkin_ws/src/ik_calculations/scripts/ik.py
+++ catkin_ws/src/ik_calculations/scripts/ik.py
@@ -30,19 +30,10 @@ def cam_to_ik(cam_coord):
   r_z_90 = np.array([ [0, -1, 0], [1, 0, 0], [0, 0, 1] ])
   return r_z_90.dot(r_y_90.dot(cam_coord))
 
-def check_coord(world_coord, ik_coord):
-  x_limit = 63
-  return (world_coord[0] + ik_coord[0]) > x_limit
-
-def rot_tt(coord):
-  r_z_90 = np.array([ [0, -1, 0], [1, 0, 0], [0, 0, 1] ])
-  return r_z_90.dot(coord)
-
 def calc_ik_horz(l3, coord, z_arm_min, z_arm_max):
   # position given is relative to base
-  y_base = coord[1];
   z_gan = z_gan_min
-  z = coord[2]
+  z = coord[1]
   x = coord[0]
   while(z - z_gan > z_arm_max):
     z_gan += 1
@@ -72,13 +63,12 @@ def calc_ik_horz(l3, coord, z_arm_min, z_arm_max):
 
   x_base = x - x_temp - x_gan
   z_gan = z - z_temp
-  return np.array([in_to_m(y_base), in_to_m(x_base), 0, in_to_m(x_gan), in_to_m(z_gan), theta1, theta2])
+  return np.array([in_to_m(x_base), in_to_m(x_gan), in_to_m(z_gan), theta1, theta2])
 
 def calc_ik_vert(l3, coord, z_arm_min, z_arm_max):
   # position given is relative to base
-  y_base = coord[1]; 
   z_gan = z_gan_min
-  z = coord[2]
+  z = coord[1]
   x = coord[0]
   while(z - z_gan > z_arm_max):
     z_gan += 1
@@ -108,43 +98,20 @@ def calc_ik_vert(l3, coord, z_arm_min, z_arm_max):
 
   x_base = x - x_temp - x_gan
   z_gan = z - z_temp
-  return np.array([in_to_m(y_base), in_to_m(x_base), 0, in_to_m(x_gan), in_to_m(z_gan), theta1, theta2])
+  return np.array([in_to_m(x_base), in_to_m(x_gan), in_to_m(z_gan), theta1, theta2])
 
-############### Tong: Please add constrains (max and min avaliable distance) on X_motion and Z_motion
 def ik_pipeline():
   # coordinates from camera in mm
-  x_cam = -500    ## Tong changed, was -300 
-  y_cam = -300     ## Tong changed, was -400 
+  x_cam = -500    ## Tong changed, was -300
   z_cam = 200    ## Tong changed, was  500
-  cam_coord = np.array([x_cam, y_cam, z_cam])
-  # current world coordinates in ik view
-  x_world = 0
-  y_world = 0
-  z_world = 0
-  world_coord = np.array([x_world, y_world, z_world])
-  print(world_coord[1])
-  # store position of turn table
-  tt_rot = False
+  cam_coord = np.array([x_cam, z_cam])
+  
   # position of end effector
   vert = False
-  #print(cam_coord)
   ik_coord = cam_to_ik(mm_to_in(cam_coord))
-  #print(ik_coord)
-  if(check_coord(world_coord, ik_coord)):
-    # rotate the turntable
-    tt_rot = True
-    ik_coord = rot_tt(ik_coord)
-  else:
-    tt_rot = False
-  #print(tt_rot)
-  #print(ik_coord)
+
   if (vert):
     ik_vals = calc_ik_vert(l3_vert, ik_coord, z_arm_min_vert, z_arm_max_vert)
   else:
     ik_vals = calc_ik_vert(l3_horz, ik_coord, z_arm_min_horz, z_arm_max_horz)
-  if(tt_rot):
-    temp = -ik_vals[1]
-    ik_vals[1] = ik_vals[0]
-    ik_vals[0] = temp
-    ik_vals[2] = np.pi/2
   return ik_vals    
