c1021ef3be0d3f3716117efb2746f5c3e14a89be
SamS1205 <snspeer@andrew.cmu.edu>
2018-04-16 16:58:01 -0400

    Updated planning node

--- planning_node.py
+++ planning_node.py
@@ -42,19 +42,30 @@ def move_turntable(station, cur_turntable_pos):
         cur_turntable_pos = 0
     return cur_turntable_pos
 
+def get_cam_coords():
+    return [0, 0, 0]
+
 if __name__ == '__main__':
     # Define current posistions of the robot
     cur_turntable_pos = 0
     cur_base_pos = [0.2286, 0.2286]
-    pathname = "all_stations.txt"
+
+    end_effector_coords = [0, 0, 0]
+    pathname = "mission_files/counterclockwise.txt"
     f = open(pathname, "r")
     commands = f.readlines()
     for s in commands:
         s = s[0:len(s)-1]
         elements = s.split(' ')
         station = elements[0][0]
+        if (station != "A" and station != "B" and station != "C" and station != "D" and station != "E" and station != "F" and station != "G" and station != "H"):
+            break
+        print("Station: " + station)
         cur_base_pos = move_to_station(station, cur_base_pos)
+        print("Base Position: [" + str(cur_base_pos[0]) + ", " + str(cur_base_pos[1]) + "]")
         cur_turntable_pos = move_turntable(station, cur_turntable_pos)
+        print("Turntable Position: " + str(cur_turntable_pos))
+        end_effector_coords = get_cam_coords()
         actuator = elements[0][1:]
         actuations = elements[1:]
         if(actuator == "A" or actuator == "B"):
