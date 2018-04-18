0e5510a5b29f7d96b5fea02dd8e86c2a51b301a6
Kevin Zhang <kevinleezhang@gmail.com>
2018-04-17 21:31:14 -0400

    Added CV stuff - Tong

--- catkin_ws/src/theboatdoctor_planner/scripts/theboatdoctor_cv.py
+++ catkin_ws/src/theboatdoctor_planner/scripts/theboatdoctor_cv.py
@@ -1,13 +1,96 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 
 
 import rospy
+import os
+import time 
 
-class TheBoatDoctorCV:
+## import ros msg
+from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import String
 
-	def __init__(self):
-		self.data = []
+## to kill terminal inside python
+from subprocess import check_output
+import signal
 
-	def get_station_info(self, actuator):
-		station_object_position_in_3d = [0,0,0,0,0,0,0,0,0]
-		station_orientation = 'up'
-		return (station_object_position_in_3d, station_orientation)
+class TheBoatDoctorCV():
+	
+	def __init__(self,Type):
+		rospy.init_node('test', anonymous=False)
+		self.type=Type
+		self.data=None
+		self.firstCall=1 
+
+	def get_actuator_location(self):
+		Type=str(self.type)
+		command="gnome-terminal -e 'rosrun object_detection kinect_client.py --type {0}'".format(Type)
+		if self.firstCall==1:
+			#os.system("gnome-terminal -e 'rosrun object_detection pi_cam_client.py --type 3'") ## call rosservice in a new terminal
+			os.system(command) ## call rosservice in a new terminal 
+			time.sleep(0.01) ## give time for os to start 
+			self.firstCall==0
+			print 3
+			msg = rospy.wait_for_message('/kinect2/actuator_location', Float32MultiArray) ## message received from publish topic in kinect 
+ 			#msg = rospy.wait_for_message('test',Bool)
+ 			print 2
+ 			self.data=msg.data
+ 			command="/home/theboatdoctor-nuc/TheBoatDoctorNUC/catkin_ws/src/object_detection/scripts/kinect_client.py --type {0}".format(Type)
+ 			pid=check_output(["pidof","python",command]) 
+ 			print pid 
+ 			#time.sleep(1000000) ## glook for pid			
+ 			os.kill(int(pid.split(" ")[0]), signal.SIGKILL)
+
+ 			if len(self.data)==1:
+ 				return 10000  ### error msg 
+ 			else:
+ 				return self.data
+ 			
+		else: 
+ 			msg = rospy.wait_for_message('/kinect2/actuator_location', Float32MultiArray) ## message received from publish topic in kinect 
+ 			self.data=msg.data
+ 			if len(self.data)==1:
+ 				return 10000  ### error msg 
+ 			else:
+ 				return self.data
+
+
+
+
+ 	def get_actuator_status(self):
+ 		Type=str(self.type)
+ 		print 1
+		command="xterm -hold -e 'rosrun object_detection pi_cam_client.py --type {0}'".format(Type)
+		#print command
+		#command="xterm -hold -e 'source /home/theboatdoctor-nuc/.bashrc && rosservice call /raspicam_service/actuator_status {0}'".format(Type)		
+		print 2
+		if self.firstCall==1:
+			print "im here"
+			os.system(command) ## call rosservice in a new terminal 
+			time.sleep(0.01)
+			self.firstCall==0
+			print 3
+			msg = rospy.wait_for_message('/raspicam_node/actuator_status',String) ## message received from publish topic in kinect 
+ 			self.data=msg.data
+ 			print 4
+ 			command="/home/theboatdoctor-nuc/TheBoatDoctorNUC/catkin_ws/src/object_detection/scripts/pi_cam_client.py --type {0}".format(Type)
+ 			pid=check_output(["pidof","python",command]) 
+ 			os.kill(int(pid.split(" ")[1]), signal.SIGKILL)
+
+ 			if len(self.data)==1:
+ 				return 10000  ### error msg 
+ 			else:
+ 				return self.data 			
+
+		else: 
+ 			msg = rospy.wait_for_message('/raspicam_node/actuator_status', String) ## message received from publish topic in kinect 
+ 			self.data=msg.data
+ 			if len(self.data)==1:
+ 				return 10000  ### error msg 
+ 			else:
+ 				return self.data
+
+
+if __name__=="__main__":
+	detector=TheBoatDoctorCV(1)
+	location=detector.get_actuator_location()
+	print location
+	print "finished"
